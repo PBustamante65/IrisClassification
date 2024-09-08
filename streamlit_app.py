2c4a819032e87d437c90d6680eb0e705a140758d
PBustamante65 <136853530+PBustamante65@users.noreply.github.com>
2024-09-08 23:20:28 +0000

    Updated directories

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ print(sys.path)
 
 #full_pipeline = joblib.load('/Volumes/TOSHIBA EXT/Maestria/Programs/Machine Learning/End-to-End Housing/exports/full_pipeline.pkl')
 
-pipiline_path = '/workspaces/IrisClassification/exports/pipeline.pkl'
+pipiline_path = 'exports/pipeline.pkl'
 with open(pipiline_path, 'rb') as file1:
     print(file1.read(100))  
 try:
@@ -33,7 +33,7 @@ try:
 except Exception as e:
     print("Failed to load pipeline:", e)
 
-model_path = '/workspaces/IrisClassification/exports/model.pkl'
+model_path = 'exports/model.pkl'
 
 with open(model_path, 'rb') as file:
     print(file.read(100))  
